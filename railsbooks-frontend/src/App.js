67e90a4a3b79a858e1d654020df1a5c107edd065
Oliver Mason <omason@protonmail.com>
2020-07-06 18:45:09 +1000

    Page changes in response to URL

--- railsbooks-frontend/src/App.js
+++ railsbooks-frontend/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import ViewBooks from './ViewBooks'
 import ViewBook from './ViewBook'
+import {BrowserRouter, Route} from 'react-router-dom'
 
 function App() {
     return (
       <div>
-        <ViewBooks />
-        <ViewBook bookId="1"/>
+        <BrowserRouter>
+          <Route exact path="/" component={ViewBooks} />  
+          <Route exact path="/books/:id" render={props => <ViewBook bookId={props.match.params.id}/> } />  
+        </BrowserRouter>
       </div>
     );
 }
