57358d282078169519af8d8d7d98bd53077823ba
Oliver Mason <omason@protonmail.com>
2020-07-06 19:03:15 +1000

    Added link to home page from individual books

--- railsbooks-frontend/src/ViewBook.js
+++ railsbooks-frontend/src/ViewBook.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
+import {Link} from 'react-router-dom'
 
 const ViewBook = (props) => {
 
@@ -25,7 +26,8 @@ const ViewBook = (props) => {
         return (
         <>
             <li key={`${book}`}>
-                {book.title} by {book.author} - {book.genre}
+                {book.title} by {book.author} - {book.genre}&nbsp;
+                <Link to="/">Book list</Link>
             </li>
         </>
         )}
